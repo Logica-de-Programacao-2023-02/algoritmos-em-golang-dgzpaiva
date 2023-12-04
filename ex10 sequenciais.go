91c80dd1632192cdb941edb2c7953cd027404ac3
dgzpaiva <143409527+dgzpaiva@users.noreply.github.com>
2023-12-03 23:08:27 -0300

    Update ex10 sequenciais.go

--- ex10 sequenciais.go
+++ ex10 sequenciais.go
@@ -1,15 +1,23 @@
 package main
 
-import (
-    "fmt"
-)
+import "fmt"
 
 func main() {
-    fmt.Print("Digite o peso em quilos: ")
-    var pesoEmKilos float64
-    fmt.Scanln(&pesoEmKilos)
+	var idade int
 
-    taxaConversao := 2.20462
-    pesoEmLibras := pesoEmKilos * taxaConversao
-    fmt.Printf("O peso em libras é: %.2f\n", pesoEmLibras)
+	fmt.Println("Digite a idade da pessoa:")
+	fmt.Scan(&idade)
+
+	switch {
+	case idade < 0:
+		fmt.Println("Idade inválida.")
+	case idade <= 12:
+		fmt.Println("Criança")
+	case idade <= 18:
+		fmt.Println("Adolescente")
+	case idade <= 59:
+		fmt.Println("Adulto")
+	default:
+		fmt.Println("Idoso")
+	}
 }
