5b2d3227e8d422c3f3fddda15e62058da63df302
dgzpaiva <143409527+dgzpaiva@users.noreply.github.com>
2023-12-03 23:16:27 -0300

    Update ex6 sequenciais.go

--- ex6 sequenciais.go
+++ ex6 sequenciais.go
@@ -8,8 +8,14 @@ func main() {
 	fmt.Println("Digite o salário do funcionário:")
 	fmt.Scan(&salario)
 
-	aumento := salario * 0.15
-	novoSalario := salario + aumento
+	var aumento, novoSalario float64
+	if salario < 1000 {
+		aumento = salario * 0.10 // Aumento de 10%
+	} else {
+		aumento = salario * 0.05 // Aumento de 5%
+	}
 
-	fmt.Printf("O novo salário com aumento é: %.2f\n", novoSalario)
+	novoSalario = salario + aumento
+
+	fmt.Printf("O novo salário é: %.2f\n", novoSalario)
 }
