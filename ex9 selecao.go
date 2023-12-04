d1fff9a99f49e086e5c67076f721ae9e662fe09a
dgzpaiva <143409527+dgzpaiva@users.noreply.github.com>
2023-12-03 23:01:07 -0300

    Create ex9 selecao.go

---
+++ ex9 selecao.go
@@ -0,0 +1,7 @@
+numero1 = float(input("Digite o primeiro número real: "))
+numero2 = float(input("Digite o segundo número real: "))
+numero3 = float(input("Digite o terceiro número real: "))
+
+numeros_ordem_crescente = sorted([numero1, numero2, numero3])
+
+print("Números em ordem crescente:", numeros_ordem_crescente)
