e508a03ddd805566d97be88e5584f39945a8cec6
dgzpaiva <143409527+dgzpaiva@users.noreply.github.com>
2023-12-03 23:10:26 -0300

    Update ex2 sequenciais.go

--- ex2 sequenciais.go
+++ ex2 sequenciais.go
@@ -3,16 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	var numero int
 
-    x := 4
-    y := 5
-    z := 7
-    if x < y && x < z {
-        fmt.Println("x é menor q y e z")
-    } else if y < x && y < z {
-        fmt.Println("y é menor q x e z")
-    } else if z < x && z < y {
-        fmt.Println("z é menor q x e y")
-    }
+	fmt.Println("Digite um número inteiro:")
+	fmt.Scan(&numero)
 
+	fmt.Printf("O dobro de %d é %d\n", numero, numero*2)
+	fmt.Printf("O triplo de %d é %d\n", numero, numero*3)
+	fmt.Printf("O quadruplo de %d é %d\n", numero, numero*4)
 }
