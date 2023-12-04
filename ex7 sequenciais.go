ad74e40101d0529da402d9c0e7a4a463ff5115c8
dgzpaiva <143409527+dgzpaiva@users.noreply.github.com>
2023-12-03 22:50:42 -0300

    Create ex7 sequenciais.go

---
+++ ex7 sequenciais.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    salario := 5000.
+    aumento := salario * 0.1
+    novosalario := salario + aumento
+    fmt.Println("novo salario é ", novosalario)
+}
