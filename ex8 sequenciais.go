98d99e94c969562f07430ac1f357babd0bc04f97
dgzpaiva <143409527+dgzpaiva@users.noreply.github.com>
2023-12-03 22:51:05 -0300

    Create ex8 sequenciais.go

---
+++ ex8 sequenciais.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+    i := 6
+    switch i {
+    case 1:
+        fmt.Println("domingo")
+    case 2:
+        fmt.Println("segunda")
+    case 3:
+        fmt.Println("terça")
+    case 4:
+        fmt.Println("quarta")
+    case 5:
+        fmt.Println("quinta")
+    case 6:
+        fmt.Println("sexta")
+    case 7:
+        fmt.Println("sabado")
+    }
+}
