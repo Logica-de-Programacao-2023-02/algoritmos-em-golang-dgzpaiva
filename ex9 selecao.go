3ae315583a25f01a709b755ad7812b941330d75d
dgzpaiva <143409527+dgzpaiva@users.noreply.github.com>
2023-12-03 23:21:26 -0300

    Update ex9 selecao.go

--- ex9 selecao.go
+++ ex9 selecao.go
@@ -6,14 +6,22 @@ import (
 )
 
 func main() {
-	var numero1, numero2, numero3 float64
+	var num1, num2, num3 float64
 
-	fmt.Println("Digite três números reais:")
-	fmt.Scan(&numero1, &numero2, &numero3)
+	fmt.Println("Digite o primeiro número:")
+	fmt.Scan(&num1)
 
-	numeros := []float64{numero1, numero2, numero3}
+	fmt.Println("Digite o segundo número:")
+	fmt.Scan(&num2)
 
+	fmt.Println("Digite o terceiro número:")
+	fmt.Scan(&num3)
+
+	numeros := []float64{num1, num2, num3}
 	sort.Float64s(numeros)
 
-	fmt.Println("Números em ordem crescente:", numeros)
+	fmt.Println("Números em ordem crescente:")
+	for _, num := range numeros {
+		fmt.Println(num)
+	}
 }
