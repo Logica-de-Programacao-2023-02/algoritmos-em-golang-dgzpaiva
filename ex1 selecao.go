51565d970760bb615135b5de9f0dc1507a1489a3
dgzpaiva <143409527+dgzpaiva@users.noreply.github.com>
2023-12-03 22:53:10 -0300

    Create ex1 selecao.go

---
+++ ex1 selecao.go
@@ -0,0 +1,10 @@
+numero1 = int(input("Digite o primeiro número inteiro: "))
+numero2 = int(input("Digite o segundo número inteiro: "))
+
+# Verificando qual número é o maior
+if numero1 > numero2:
+    print(f"O maior número é: {numero1}")
+elif numero2 > numero1:
+    print(f"O maior número é: {numero2}")
+else:
+    print("Os números são iguais.")
