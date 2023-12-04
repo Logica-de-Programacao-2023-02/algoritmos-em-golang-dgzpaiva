37daab1fd6117592fc8db57cce0358596db5f387
dgzpaiva <143409527+dgzpaiva@users.noreply.github.com>
2023-12-03 23:17:23 -0300

    Update ex7 sequenciais.go

--- ex7 sequenciais.go
+++ ex7 sequenciais.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	var numero int
 
-    salario := 5000.
-    aumento := salario * 0.1
-    novosalario := salario + aumento
-    fmt.Println("novo salario é ", novosalario)
+	fmt.Println("Digite um número inteiro:")
+	fmt.Scan(&numero)
+
+	sucessor := numero + 1
+	antecessor := numero - 1
+
+	fmt.Printf("O número digitado é: %d\n", numero)
+	fmt.Printf("Seu antecessor é: %d\n", antecessor)
+	fmt.Printf("Seu sucessor é: %d\n", sucessor)
 }
